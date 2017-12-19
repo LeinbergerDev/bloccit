63d6dfe3bc7971eb956f7a6185b4d3fca87840cb
jason@leinberberdev.com <jason@leinbergerdev.com>
2017-12-19 10:54:33 -0600

    assignment 18 CRUD2: updated

--- spec/controllers/advertisements_controller_spec.rb
+++ spec/controllers/advertisements_controller_spec.rb
@@ -51,16 +51,16 @@ RSpec.describe AdvertisementsController, type: :controller do
 
   describe "POST #create" do
     it "increases the number of advertisement by 1" do
-      expect{ advertisement :create, params: { advertisement: { title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 10 }}}.to change(Advertisement, :count).by(1)
+      expect{ post :create, params: { advertisement: { title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 10 }}}.to change(Advertisement, :count).by(1)
     end
 
     it "assigns the new advertisement to @advertisement" do
-      advertisement :create, params: { advertisement: { title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 10}}
+      post :create, params: { advertisement: { title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 10}}
       expect(assigns(:advertisement)).to eq Advertisement.last
     end
 
     it "redirects to the new advertisement" do
-      advertisement :create, params: { advertisement: { title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 10}}
+      post :create, params: { advertisement: { title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 10}}
       expect(response).to redirect_to Advertisement.last
     end
   end
