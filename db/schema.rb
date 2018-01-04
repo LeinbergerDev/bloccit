286ed9d2ccf29eda3772ff03862897772299d0a2
jason@leinberberdev.com <jason@leinbergerdev.com>
2018-01-03 22:38:17 -0600

    checkpoint 27 authorization: complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171230045919) do
+ActiveRecord::Schema.define(version: 20180103172419) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20171230045919) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
